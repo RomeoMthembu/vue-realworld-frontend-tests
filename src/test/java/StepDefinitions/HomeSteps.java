bb0b029678dc3a5e6f7371bfb1ce7a68c49f16dc
RomeoMthembu <romeomthembu@gmail.com>
2025-04-11 16:02:21 +0200

    Added Web Title Validation

--- src/test/java/StepDefinitions/HomeSteps.java
+++ src/test/java/StepDefinitions/HomeSteps.java
@@ -1,35 +1,63 @@
 package StepDefinitions;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.concurrent.TimeUnit;
+
+import static junit.framework.Assert.assertEquals;
 
 
 public class HomeSteps {
 
     WebDriver webDriver = null;
 
-    @Given("A user launches Conduit website")
-    public void a_user_launches_conduit_website() throws InterruptedException {
+
+   @Before
+    public void start() throws InterruptedException {
+
         System.setProperty("webdriver.firefox.driver", "/opt/homebrew/bin/geckodriver.exe");
         webDriver = new FirefoxDriver();
         webDriver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         webDriver.manage().window().maximize();
+
+
+
+    }
+
+    @After
+    public  void End (){
+
+        webDriver.quit();
+
+    }
+
+    @Given("A user launches Conduit website")
+    public void a_user_launches_conduit_website() throws InterruptedException {
+
         webDriver.get("http://localhost:8080/#/");
-        Thread.sleep(5000);
+
+
     }
 
     @When("The page has fully loaded")
-    public void the_page_has_fully_loaded() {
+    public void the_page_has_fully_loaded() throws InterruptedException {
+        Thread.sleep(5000);
 
     }
 
     @Then("Verify the page title is correctly displayed")
-    public void verify_the_page_title_is_correctly_displayed() {
+    public void verify_the_page_title_is_correctly_displayed() throws InterruptedException {
 
-        webDriver.quit();
+        String actualTitle = webDriver.getTitle();
+        String expectedTitle = "Conduit";
+
+        assertEquals("Title does not match the expected value", actualTitle, expectedTitle);
+        Thread.sleep(5000);
 
     }
 }
